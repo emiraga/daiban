b6a85957207cad5ba02976d22622aea530e846ec
Emir Bosnian <emiraga@gmail.com>
2026-03-28 19:11:41 +0800

    Initial Commit

---
+++ daiban/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  daiban
+//
+//  Created by Emir B on 3/28/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ daibanTests/daibanTests.swift
@@ -0,0 +1,18 @@
+//
+//  daibanTests.swift
+//  daibanTests
+//
+//  Created by Emir B on 3/28/26.
+//
+
+import Testing
+
+struct daibanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
